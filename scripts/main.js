043b39d48cb34f61ccd29fc3c078fc8e9c9b746e
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2020-02-28 17:20:55 -0300

    versionando

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$('#menuAdm a[href*="#"]').on('click', function(e) {
+    e.preventDefault()
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top-90,
+      },
+      500,
+      'linear'
+    )
+  })
+
+  
